a7ef9e96af5ff10885d99019617922aa4698f399
thupas <siriyalapavankumar>
2023-06-27 11:52:26 +0530

    frist commit

---
+++ js/script.js
@@ -0,0 +1,134 @@
+var firstSeatLabel = 1;
+var booked = !!localStorage.getItem('booked') ? $.parseJSON(localStorage.getItem('booked')) : [];
+$(document).ready(function() {
+    var $cart = $('#selected-seats'),
+        $counter = $('#counter'),
+        $total = $('#total'),
+        sc = $('#bus-seat-map').seatCharts({
+            map: [
+                'ff_ff',
+                'ef_fe',
+                'fe_fe',
+                'ee_ee',
+                'ee___',
+                'ee_ee',
+                'ff-',
+                'ee_ee',
+                'eeeem',
+            ],
+            seats: {
+                f: {
+                    price: 750,
+                    classes: 'first-class', //your custom CSS class
+                    category: 'First Class'
+                },
+                e: {
+                    price: 750,
+                    classes: 'economy-class', //your custom CSS class
+                    category: 'Economy Class'
+                }
+
+            },
+            naming: {
+                top: false,
+                getLabel: function(character, row, column) {
+                    return firstSeatLabel++;
+                },
+            },
+            legend: {
+                node: $('#legend'),
+                items: [
+                    ['f', 'available', ' Avaiable for Male'],
+                    ['e', 'available', ' Avaiable for Female'],
+                    ['f', 'unavailable', 'Already Booked']
+                ]
+            },
+            click: function() {
+                if (this.status() == 'available') {
+                   
+                    $('<li>' + this.data().category + ' Seat # ' + this.settings.label + ': <b>$' + this.data().price + '</b> <a href="#" class="cancel-cart-item">[cancel]</a></li>')
+                        .attr('id', 'cart-item-' + this.settings.id)
+                        .data('seatId', this.settings.id)
+                        .appendTo($cart);
+
+                   
+                    $counter.text(sc.find('selected').length + 1);
+                    $total.text(recalculateTotal(sc) + this.data().price);
+
+                    return 'selected';
+
+                } else if (this.status() == 'selected') {
+
+                    //update the counter
+                    $counter.text(sc.find('selected').length - 1);
+
+                    //and total
+                    $total.text(recalculateTotal(sc) - this.data().price);
+
+                    //remove the item from our cart
+                    $('#cart-item-' + this.settings.id).remove();
+
+                    //seat has been vacated
+                    return 'available';
+
+                } else if (this.status() == 'unavailable') {
+                    //seat has been already booked
+                    return 'unavailable';
+                } else {
+                    return this.style();
+                }
+            }
+        });
+
+    //this will handle "[cancel]" link clicks
+    $('#selected-seats').on('click', '.cancel-cart-item', function() {
+        //let's just trigger Click event on the appropriate seat, so we don't have to repeat the logic here
+        sc.get($(this).parents('li:first').data('seatId')).click();
+    });
+
+    
+    sc.get(booked).status('unavailable');
+
+});
+
+function recalculateTotal(sc) {
+    var total = 0;
+    sc.find('selected').each(function() {
+
+        total += this.data().price;
+
+    });
+
+    return total;
+}
+
+$(function() {
+    $('#checkout-button').click(function() {
+        var items = $('#selected-seats li')
+        if (items.length <= 0) {
+            alert("Please select atleast 1 seat first.")
+            return false;
+        }
+        var selected = [];
+        items.each(function(e) {
+            var id = $(this).attr('id')
+            id = id.replace("cart-item-", "")
+            selected.push(id)
+        })
+        if (Object.keys(booked).length > 0) {
+            Object.keys(booked).map(k => {
+                selected.push(booked[k])
+            })
+        }
+        localStorage.setItem('booked', JSON.stringify(selected))
+        alert("Seats has been Reserved successfully.")
+        location.reload()
+    })
+    $('#reset-btn').click(function() {
+        if (confirm("are you sure to reset the reservation of the bus?") === true) {
+            localStorage.removeItem('booked')
+            alert("Seats has been Reset successfully.")
+            location.reload()
+        }
+    })
+})
